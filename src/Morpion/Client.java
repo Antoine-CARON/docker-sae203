162259e207637c5c542ba0ed8ffac1201ffb75e2
Antoine CARON <103735534+Antoine-CARON@users.noreply.github.com>
2023-05-25 15:30:46 +0200

    Create Client.java

---
+++ src/Morpion/Client.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+	private Socket toServer;
+
+	public Client( String host)
+	{		
+		try
+		{
+			this.toServer = new Socket( host, 4660 );
+		}
+		catch( IOException e )
+		{
+			System.out.println("Erreur");
+		}
+
+	}
+
+    public static void main(String[] args) {
+        new Client(args[0]);
+    }
+}
